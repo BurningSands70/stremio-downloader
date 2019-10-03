4dcbb8c868bd32495e8bcf000a5b0ac15a56f510
BurningSands70 <55944558+BurningSands70@users.noreply.github.com>
2019-10-03 13:49:34 +0300

    Fix critical bug with passing filename 2

--- downloader/script.js
+++ downloader/script.js
@@ -12,23 +12,23 @@ function fileToRow(file, idx) {
 		'<td class="actions">'
 
 	str += '' +
-		'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" onClick="apiCall(\'remove-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename)+'\')">' +
+		'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" onClick="apiCall(\'remove-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename).replace(/'/g, "%27")+'\')">' +
 			'<i class="material-icons">delete</i>' +
 		'</button>'
 
 	if (file.finished) {
 		str += '' +
-			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="fileOptions(\''+file.url+'\', \''+encodeURIComponent(file.filename)+'\')">' +
+			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="fileOptions(\''+file.url+'\', \''+encodeURIComponent(file.filename).replace(/'/g, "%27")+'\')">' +
 				'<i class="material-icons">menu</i>' +
 			'</button>'
 	} else if (file.error || file.stopped) {
 		str += '' +
-			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="apiCall(\'restart-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename)+'\')">' +
+			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="apiCall(\'restart-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename).replace(/'/g, "%27")+'\')">' +
 				'<i class="material-icons">refresh</i>' +
 			'</button>'
 	} else {
 		str += '' +
-			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="apiCall(\'stop-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename)+'\')">' +
+			'<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="margin-left: 14px; margin-right: 0" onClick="apiCall(\'stop-download\', \''+file.url+'\', \''+encodeURIComponent(file.filename).replace(/'/g, "%27")+'\')">' +
 				'<i class="material-icons">stop</i>' +
 			'</button>'
 	}
